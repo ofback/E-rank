c990dac5ecff42675d89c6394be31daccd9a15fd
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2025-03-21 13:16:16 -0300

    adição da annotations nas tabelas estatísticas e desafio

--- src/main/java/com/doback/E_rank/entity/Desafio.java
+++ src/main/java/com/doback/E_rank/entity/Desafio.java
@@ -1,47 +1,63 @@
 package com.doback.E_rank.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="desafios")
+
 public class Desafio {
-    private Long IdDesafio;
-    private Long IdUsuario1;
-    private Long IdUsuario2;
-    private Long Status;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "idDesafio")
+    private Long idDesafio;
+    @Column(name = "idUsuario1")
+    private Long idUsuario1;
+    @Column(name = "idUsuario2")
+    private Long idUsuario2;
+    @Column(name = "status")
+    private Long status;
 
     public Desafio(Long idDesafio, Long idUsuario1, Long idUsuario2, Long status) {
-        IdDesafio = idDesafio;
-        IdUsuario1 = idUsuario1;
-        IdUsuario2 = idUsuario2;
-        Status = status;
+        this.idDesafio = idDesafio;
+        this.idUsuario1 = idUsuario1;
+        this.idUsuario2 = idUsuario2;
+        this.status = status;
+    }
+
+    public Desafio() {
+
     }
 
     public Long getIdDesafio() {
-        return IdDesafio;
+        return idDesafio;
     }
 
     public void setIdDesafio(Long idDesafio) {
-        IdDesafio = idDesafio;
+        this.idDesafio = idDesafio;
     }
 
     public Long getIdUsuario1() {
-        return IdUsuario1;
+        return idUsuario1;
     }
 
     public void setIdUsuario1(Long idUsuario1) {
-        IdUsuario1 = idUsuario1;
+        this.idUsuario1 = idUsuario1;
     }
 
     public Long getIdUsuario2() {
-        return IdUsuario2;
+        return idUsuario2;
     }
 
     public void setIdUsuario2(Long idUsuario2) {
-        IdUsuario2 = idUsuario2;
+        this.idUsuario2 = idUsuario2;
     }
 
     public Long getStatus() {
-        return Status;
+        return status;
     }
 
     public void setStatus(Long status) {
-        Status = status;
+        this.status = status;
     }
 }
--- src/main/java/com/doback/E_rank/entity/Estatistica.java
+++ src/main/java/com/doback/E_rank/entity/Estatistica.java
@@ -1,10 +1,24 @@
 package com.doback.E_rank.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="estatísticas")
+
 public class Estatistica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id_estatistica")
     private Long id_estatistica;
+    @Column(name = "kilss")
     private Long kills;
+    @Column(name = "assistencias")
     private int assistencias;
+    @Column(name = "qts_partidas")
     private int qts_partidas;
+    @Column(name = "sts_provacao")
     private int sts_provacao;
 
     public Estatistica(Long id_estatistica, Long kills, int assistencias, int qts_partidas, int sts_provacao) {
@@ -13,6 +27,10 @@ public class Estatistica {
         this.assistencias = assistencias;
         this.qts_partidas = qts_partidas;
         this.sts_provacao = sts_provacao;
+    }
+
+    public Estatistica() {
+
     }
 
     public Long getId_estatistica() {
