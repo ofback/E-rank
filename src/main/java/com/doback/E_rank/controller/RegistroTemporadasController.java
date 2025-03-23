baff5f0850e2adfa8037e4f6e43ef44a3ebbbd02
Gabriel Alves <gabriel12425@gmail.com>
2025-03-23 14:34:18 -0300

    Criação das camadas referentes as entidades registroTemporadas e registroTimes

---
+++ src/main/java/com/doback/E_rank/controller/RegistroTemporadasController.java
@@ -0,0 +1,41 @@
+package com.doback.E_rank.controller;
+import com.doback.E_rank.entity.RegistroTemporadas;
+import com.doback.E_rank.facade.RegistroTemporadasFacade;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/times")
+public class RegistroTemporadasController {
+
+    private final RegistroTemporadasFacade registroTemporadasFacade;
+
+    public RegistroTemporadasController(RegistroTemporadasFacade registroTemporadasFacade) {
+        this.registroTemporadasFacade = registroTemporadasFacade;
+    }
+
+    @GetMapping
+    public List<RegistroTemporadas> listarRegistroTemporadas() {
+        return registroTemporadasFacade.listarRegistroTemporadas();
+    }
+
+    @GetMapping("/{id}")
+    public RegistroTemporadas obterRegistroTemporadasPorId(@PathVariable Long id) {
+        return registroTemporadasFacade.buscarRegistroTemporadasPorId(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarRegistroTemporadas(@RequestBody RegistroTemporadas registroTemporadas) {
+        registroTemporadasFacade.salvarRegistroTemporadas(registroTemporadas);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluirRegistroTemporadas(@PathVariable Long id) {
+        registroTemporadasFacade.excluirRegistroTemporadas(id);
+    }
+
+}
--- src/main/java/com/doback/E_rank/repository/RegistroTemporadasRepository.java
+++ src/main/java/com/doback/E_rank/repository/RegistroTemporadasRepository.java
@@ -1,4 +1,32 @@
 package com.doback.E_rank.repository;
+import com.doback.E_rank.entity.RegistroTemporadas;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
 
+@Repository
 public class RegistroTemporadasRepository {
+    private List<RegistroTemporadas> registroTemporadas = new ArrayList<>();
+
+    public RegistroTemporadas buscarPorId(long code) {
+        RegistroTemporadas registroTemporada = registroTemporadas
+                .stream()
+                .filter(p -> p.getId_registro_temporadas() == code)
+                .findFirst()
+                .get();
+
+        return registroTemporada;
+    }
+
+    public List<RegistroTemporadas> buscar(){
+        return registroTemporadas;
+    }
+
+    public void adicionar(RegistroTemporadas registroTemporada) {
+        registroTemporadas.add(registroTemporada);
+    }
+
+    public void remover(long code){
+        registroTemporadas.removeIf(p -> p.getId_registro_temporadas() == code);
+    }
 }
