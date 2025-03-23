86c9501afeaedb214615d49ff31b11acfc9d4163
Matheus Amaral <matheus.ratto11@gmail.com>
2025-03-23 13:49:59 -0300

    Adicionando tabelas restantes

---
+++ src/main/java/com/doback/E_rank/facade/FeedMensagensFacade.java
@@ -0,0 +1,36 @@
+package com.doback.E_rank.facade;
+import com.doback.E_rank.application.FeedMensagensApplication;
+import com.doback.E_rank.entity.FeedMensagens;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+
+public class FeedMensagensFacade {
+    private final FeedMensagensApplication feedMensagensApplication;
+
+    public FeedMensagensFacade(FeedMensagensApplication feedMensagensApplication) {
+        this.feedMensagensApplication = feedMensagensApplication;
+    }
+
+    public List<FeedMensagens> listarFeedMensagens() {
+        return feedMensagensApplication.obterTodosFeedMensagens();
+    }
+
+    public FeedMensagens buscarFeedMensagensPorId(Long id) {
+        return feedMensagensApplication.obterFeedMensagensPorId(id);
+    }
+
+    public void salvarFeedMensagens(FeedMensagens feedMensagens) {
+        feedMensagensApplication.criarFeedMensagens(feedMensagens);
+    }
+
+    public void excluirFeedMensagens(Long id) {
+        feedMensagensApplication.excluirFeedMensagens(id);
+    }
+
+    public void atualizarFeedMensagens(Long id, FeedMensagens feedMensagens) {
+        feedMensagensApplication.atualizarFeedMensagens(id, feedMensagens);
+    }
+}
