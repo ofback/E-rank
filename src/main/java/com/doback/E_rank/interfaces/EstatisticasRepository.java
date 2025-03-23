e8b80e256160f28c621010929383da8b42c1407f
phznx7 <pedro.h.santos20@ba.estudante.senai.br>
2025-03-23 16:18:21 -0700

    Implementação JpaRepository na entidade Estatisticas
    # Conflicts:
    #	src/main/java/com/doback/E_rank/interfaces/EstatisticasRepository.java

--- src/main/java/com/doback/E_rank/interfaces/EstatisticasRepository.java
+++ src/main/java/com/doback/E_rank/interfaces/EstatisticasRepository.java
@@ -1,16 +1,4 @@
 package com.doback.E_rank.interfaces;
 
-import com.doback.E_rank.entity.Estatisticas;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
 public interface EstatisticasRepository {
-    public Estatisticas searchByCode(int id);
-    public List<Estatisticas> buscar();
-    public void addEstatisticas(Estatisticas estatisticas);
-    public void removeEstatisticas(int id);
-    public void updateEstatisticas(int id, Estatisticas estatisticas);
-    public boolean estaVazio();
-
 }
--- src/main/java/com/doback/E_rank/repository/jpa/DesafiosJpa.java
+++ src/main/java/com/doback/E_rank/repository/jpa/DesafiosJpa.java
@@ -1,4 +1,7 @@
 package com.doback.E_rank.repository.jpa;
 
-public interface DesafiosJpa {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.doback.E_rank.entity.Desafios;
+
+public interface DesafiosJpa extends JpaRepository<Desafios, Integer> {
 }
