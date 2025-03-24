d45e1bb5fff2f395156d92e7db40828f43341df0
Pedro Henrique <phthedevx@gmail.com>
2025-03-24 00:47:35 -0700

    continue rebase

--- src/main/java/com/doback/E_rank/interfaces/EstatisticasRepository.java
+++ src/main/java/com/doback/E_rank/interfaces/EstatisticasRepository.java
@@ -1,4 +1,15 @@
 package com.doback.E_rank.interfaces;
 
+import com.doback.E_rank.entity.Estatisticas;
+
+import java.util.List;
+
 public interface EstatisticasRepository {
+    public Estatisticas searchByCode(int id);
+    public List<Estatisticas> buscar();
+    public void addEstatisticas(Estatisticas estatisticas);
+    public void removeEstatisticas(int id);
+    public void updateEstatisticas(int id, Estatisticas estatisticas);
+    public boolean estaVazio();
+
 }
