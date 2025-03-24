8f4d5161f573a5de55f23a12c69a19917e0a3f5f
phznx7 <pedro.h.santos20@ba.estudante.senai.br>
2025-03-24 00:34:24 -0700

    Implementação JpaRepository na entidade Desafios.java

--- src/main/java/com/doback/E_rank/interfaces/DesafiosRepository.java
+++ src/main/java/com/doback/E_rank/interfaces/DesafiosRepository.java
@@ -1,4 +1,13 @@
 package com.doback.E_rank.interfaces;
 
+import com.doback.E_rank.entity.Desafios;
+import java.util.List;
+
 public interface DesafiosRepository {
+    public Desafios searchByCode(int code);
+    public List<Desafios> buscar();
+    public void addDesafios(Desafios desafios);
+    public void removeDesafios(int code);
+    public void updateDesafios(int code, Desafios desafios);
+    public boolean estaVazio();
 }
