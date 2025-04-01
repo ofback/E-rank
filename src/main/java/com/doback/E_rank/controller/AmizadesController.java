191ee688f10fbc388417baff0b677054e39b8891
Matheus Amaral <matheus.ratto11@gmail.com>
2025-04-01 15:38:57 -0300

    padronização de requestMapping

--- src/main/java/com/doback/E_rank/controller/AmizadesController.java
+++ src/main/java/com/doback/E_rank/controller/AmizadesController.java
@@ -1,7 +1,6 @@
 package com.doback.E_rank.controller;
 
 import com.doback.E_rank.entity.Amizades;
-import com.doback.E_rank.entity.Times;
 import com.doback.E_rank.facade.AmizadesFacade;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/doback/E_rank/controller/TemporadasController.java
+++ src/main/java/com/doback/E_rank/controller/TemporadasController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Temporadas")
+@RequestMapping("/temporadas")
 public class TemporadasController {
 
     private final TemporadasFacade temporadaFacade;
