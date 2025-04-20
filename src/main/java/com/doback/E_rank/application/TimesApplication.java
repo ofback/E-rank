d494e080d91bdff1964fd62a1a278312ccf9c003
phznx7 <pedro.h.santos20@ba.estudante.senai.br>
2025-04-20 14:51:16 -0700

    Criação de método de validação na classe Times.java

--- src/main/java/com/doback/E_rank/application/TimesApplication.java
+++ src/main/java/com/doback/E_rank/application/TimesApplication.java
@@ -1,4 +1,6 @@
 package com.doback.E_rank.application;
+
+import com.doback.E_rank.entity.Times;
 import com.doback.E_rank.models.TimesModel;
 import com.doback.E_rank.interfaces.TimesRepository;
 import org.springframework.stereotype.Service;
@@ -14,23 +16,58 @@ public class TimesApplication {
         this.timesRepository = timesRepository;
     }
 
+
     public List<TimesModel> obterTodosTimes() {
         return timesRepository.buscar();
     }
 
+
     public TimesModel obterTimesPorId(int id) {
-        return timesRepository.searchByCode(id);
+        TimesModel timesModel = timesRepository.searchByCode(id);
+        if (timesModel == null) {
+            throw new IllegalArgumentException("Time não encontrado.");
+        }
+        return timesModel;
     }
 
+
     public void criarTime(TimesModel timesModel) {
+        Times timeEntidade = new Times(
+                timesModel.getNome(),
+                timesModel.getDescricao(),
+                timesModel.getSts(),
+                timesModel.getIdTemporada(),
+                timesModel.getIdUsuario()
+        );
+
+        if (!timeEntidade.validarTime()) {
+            throw new IllegalArgumentException("Validação do time falhou: " + timeEntidade.getErrosValidacao());
+        }
+
         timesRepository.addTimes(timesModel);
     }
 
-    public void excluirTime(int id) {
-        timesRepository.removeTimes(id);
-    }
+
     public void atualizarTimes(int id, TimesModel timesModel) {
+        Times timeEntidade = new Times(
+                timesModel.getNome(),
+                timesModel.getDescricao(),
+                timesModel.getSts(),
+                timesModel.getIdTemporada(),
+                timesModel.getIdUsuario()
+        );
+
+        if (!timeEntidade.validarTime()) {
+            throw new IllegalArgumentException("Validação do time falhou: " + timeEntidade.getErrosValidacao());
+        }
+
         timesRepository.updateTimes(id, timesModel);
+    }
+
+
+    public void excluirTime(int id) {
+        TimesModel timesModel = obterTimesPorId(id);
+        timesRepository.removeTimes(timesModel.getId());
     }
 
 
