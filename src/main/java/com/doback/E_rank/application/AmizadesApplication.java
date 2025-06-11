43f0701afcac04df55662544b22dbb5844fb7f67
phthedev <pedro.h.santos20@ba.estudante.senai.br>
2025-06-11 18:53:39 -0300

    fix: implementação inicial do sistema de notificações com factory method

--- src/main/java/com/doback/E_rank/application/AmizadesApplication.java
+++ src/main/java/com/doback/E_rank/application/AmizadesApplication.java
@@ -6,13 +6,18 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import com.doback.E_rank.application.NotificacaoApplication;
+
 @Service
 public class AmizadesApplication {
 
     private final AmizadesRepository amizadeRepository;
+    private final NotificacaoApplication notificacaoApplication;
 
-    public AmizadesApplication(AmizadesRepository amizadeRepository) {
+
+    public AmizadesApplication(AmizadesRepository amizadeRepository, NotificacaoApplication notificacaoApplication) {
         this.amizadeRepository = amizadeRepository;
+        this.notificacaoApplication = notificacaoApplication;
     }
 
     public List<AmizadesModel> obterTodasAmizades() {
@@ -25,16 +30,23 @@ public class AmizadesApplication {
 
     public void criarAmizade(AmizadesModel amizadesModel) {
         validar(amizadesModel);
+
         amizadeRepository.addAmizades(amizadesModel);
+
+        String mensagem = "Você recebeu um novo pedido de amizade do usuário " + amizadesModel.getIdUsuario1();
+        String destinatario = "usuario_id:" + amizadesModel.getIdUsuario2(); // Exemplo de identificador
+        notificacaoApplication.enviarNotificacao("sistema", mensagem, destinatario);
     }
 
     public void excluirAmizade(int id) {
         amizadeRepository.removeAmizades(id);
+        // Adicionar uma notificação de amizade removida
     }
 
     public void atualizarAmizades(int id, AmizadesModel amizadesModel) {
         validar(amizadesModel);
         amizadeRepository.updateAmizades(id, amizadesModel);
+        // Poderia notificar sobre a amizade ter sido aceita, por exemplo
     }
 
     private Amizades validar(AmizadesModel amizadesModel){
