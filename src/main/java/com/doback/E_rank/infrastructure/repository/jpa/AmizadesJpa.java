f165fa1b2934e64409940e8c28976a1b04330aad
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2025-10-05 12:47:22 -0300

    feat(amizades): adiciona métodos de busca por usuário e status no repositório

--- src/main/java/com/doback/E_rank/infrastructure/repository/jpa/AmizadesJpa.java
+++ src/main/java/com/doback/E_rank/infrastructure/repository/jpa/AmizadesJpa.java
@@ -2,6 +2,13 @@ package com.doback.E_rank.infrastructure.repository.jpa;
 
 import com.doback.E_rank.infrastructure.models.AmizadesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List; // Importar a List
 
 public interface AmizadesJpa extends JpaRepository<AmizadesModel, Integer> {
+    // Encontra amizades onde o usuário logado é o solicitante (usuario1) OU o solicitado (usuario2)
+    // e o status é o que foi passado como parâmetro.
+    List<AmizadesModel> findByIdUsuario1AndStatusOrIdUsuario2AndStatus(int idUsuario1, char status1, int idUsuario2, char status2);
+
+    // Encontra amizades onde o usuário logado é o solicitado (usuario2) e o status é pendente
+    List<AmizadesModel> findByIdUsuario2AndStatus(int idUsuario2, char status);
 }
