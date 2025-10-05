616122901245d8d8603fb847b23e55c864e51c47
Pedro Henrique <phthedevx@gmail.com>
2025-10-05 16:13:48 -0300

    feat(dto): cria UpdateFriendshipStatusDTO para aceitar/recusar amizades

---
+++ src/main/java/com/doback/E_rank/dto/UpdateFriendshipStatusDTO.java
@@ -0,0 +1,10 @@
+package com.doback.E_rank.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateFriendshipStatusDTO {
+    private char status; // Ex: 'A' para Aceito
+}
