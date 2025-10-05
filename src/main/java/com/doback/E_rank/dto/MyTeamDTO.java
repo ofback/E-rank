8f5631d3f664a264723bc6fd50fca9ca315d3eba
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2025-10-05 19:16:03 -0300

    .

---
+++ src/main/java/com/doback/E_rank/dto/MyTeamDTO.java
@@ -0,0 +1,10 @@
+package com.doback.E_rank.dto;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class MyTeamDTO {
+    private int teamId;
+    private String teamName;
+    private String userRole;
+    private String userStatus;
+}
