c38df98a2ed138981dfde43508e7f50cf24e4f7b
Pedro Henrique <phthedevx@gmail.com>
2025-10-05 21:15:52 -0300

    refactor(repository): adiciona metodos de busca e delecao em RegistroTimesJpa

--- src/main/java/com/doback/E_rank/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/doback/E_rank/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.doback.E_rank.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
@@ -22,5 +23,12 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public class ResourceNotFoundException extends RuntimeException {
+        public ResourceNotFoundException(String message) {
+            super(message);
+        }
 
+
+    }
 }
