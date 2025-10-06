4c1f42319a002266888b97ce96979ec40dc0120f
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2025-10-05 21:15:52 -0300

    refactor(repository): adiciona metodos de busca e delecao em RegistroTimesJpa

--- src/main/java/com/doback/E_rank/infrastructure/repository/jpa/RegistroTimesJpa.java
+++ src/main/java/com/doback/E_rank/infrastructure/repository/jpa/RegistroTimesJpa.java
@@ -2,8 +2,16 @@ package com.doback.E_rank.infrastructure.repository.jpa;
 
 import com.doback.E_rank.infrastructure.models.RegistroTimesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface RegistroTimesJpa extends JpaRepository<RegistroTimesModel, Integer> {
     List<RegistroTimesModel> findByIdUsuarios(int userId);
+
+    Optional<RegistroTimesModel> findByIdTimesAndIdUsuarios(int idTimes, int idUsuarios);
+
+    @Transactional
+    void deleteByIdTimesAndIdUsuarios(int idTimes, int idUsuarios);
 }
