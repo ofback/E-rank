ea9087c8b229bddfa4adc7c602129b6bf5708fa8
Pedro Henrique <phthedevx@gmail.com>
2025-11-19 14:57:46 -0300

    feat(back): secure team update with DTO and partial merge logic

---
+++ src/main/java/com/doback/E_rank/dto/UpdateTeamDTO.java
@@ -0,0 +1,12 @@
+// E-rank/src/main/java/com/doback/E_rank/dto/UpdateTeamDTO.java
+package com.doback.E_rank.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateTeamDTO {
+    private String nome;
+    private String descricao;
+}
